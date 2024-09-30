831cffd2c1f4a8fe547b033e17ea06f40d8b8a8e
josehs <you@example.com>
2024-09-30 20:41:56 +0200

    De git a github

--- src/main/java/com/josehs/tema03/Ejercicio1.java
+++ src/main/java/com/josehs/tema03/Ejercicio1.java
@@ -3,5 +3,6 @@ package com.josehs.tema03;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Ejercicio1");
+        System.out.println("Archivo de git en github");
     }
 }

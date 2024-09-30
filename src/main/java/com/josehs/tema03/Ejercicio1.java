212de709e92f40364a0d7fad70406d5008203f8d
josehs <you@example.com>
2024-09-30 20:11:58 +0200

    Añadido main a Ejercicio1

--- src/main/java/com/josehs/tema03/Ejercicio1.java
+++ src/main/java/com/josehs/tema03/Ejercicio1.java
@@ -2,6 +2,6 @@ package com.josehs.tema03;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Primera practica de git");
+        System.out.println("Ejercicio1");
     }
 }

a84f97df136a1dcd27d57e3465dcbc21ec7c9a29
josehs <you@example.com>
2024-10-01 17:06:13 +0200

    Ejercio3T3

---
+++ src/main/java/com/josehs/tema03/EjercicioT3_3.java
@@ -0,0 +1,16 @@
+package com.josehs.tema03;
+
+import java.util.Scanner;
+
+public class EjercicioT3_3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime tu edad");
+        int edad = teclado.nextInt();
+        if (edad>17) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
+    }
+}

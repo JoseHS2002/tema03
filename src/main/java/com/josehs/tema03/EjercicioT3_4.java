a4117c40b7192002c104108a3cf1a4df0b0bca9c
josehs <you@example.com>
2024-10-02 10:27:02 +0200

    Ejercio4T3

---
+++ src/main/java/com/josehs/tema03/EjercicioT3_4.java
@@ -0,0 +1,18 @@
+package com.josehs.tema03;
+
+import java.util.Scanner;
+
+public class EjercicioT3_4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el primer numero");
+        int primerNumero = teclado.nextInt();
+        System.out.println("Escribe el segundo numero");
+        int segundoNumero = teclado.nextInt();
+        if (primerNumero>segundoNumero) {
+            System.out.println(primerNumero + ", " + segundoNumero);
+        } else if (primerNumero<segundoNumero) {
+            System.out.println(segundoNumero + ", " + primerNumero);
+        }
+    }
+}

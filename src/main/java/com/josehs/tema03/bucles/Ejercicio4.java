3c5dd655c32a07324ab50401f4f31fc93a25380d
josehs <you@example.com>
2024-10-07 17:24:47 +0200

    Ejercio5

--- src/main/java/com/josehs/tema03/bucles/Ejercicio4.java
+++ src/main/java/com/josehs/tema03/bucles/Ejercicio4.java
@@ -2,16 +2,16 @@ package com.josehs.tema03.bucles;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int parImpar;
+
         int par=0;
         int impar=0;
         System.out.println("Contar hasta 100 y decir cuantos son pares e impares");
-        for (parImpar=1) {
+        for (int parImpar=0; parImpar<101; parImpar++) {
             System.out.println(parImpar);
-            if (parImpar % 2=0) {
-                par == par +1;
+            if (parImpar%0) {
+                par = par +1;
             } else {
-                impar == impar +1;
+                impar = impar +1;
             }
 
         }
--- src/main/java/com/josehs/tema03/bucles/Ejercicio5.java
+++ src/main/java/com/josehs/tema03/bucles/Ejercicio5.java
@@ -2,18 +2,19 @@ package com.josehs.tema03.bucles;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        int = par_impar;
+
         int par=0;
         int impar=0;
-        System.out.println("Contar hasta 100 y decir cuantos son pares e impares");
-        for (par_impar=0; 100; 1) {
-            System.out.println(par_impar);
-            if (par_impar % 2=0) {
-                par == par +1;
-                System.out.println(par+"es par");
+        System.out.println("Contar hasta 100 y decir cuales son pares");
+        for (int parImpar=0; parImpar<101; parImpar++) {
+            System.out.println(parImpar);
+            if (parImpar%0) {
+                par = par +1;
+                System.out.println(par+" es par");
             } else {
-                impar == impar +1;
+                impar = impar +1;
             }
+
         }
     }
 }

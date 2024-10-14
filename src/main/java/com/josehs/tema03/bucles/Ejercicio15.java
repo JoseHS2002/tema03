f08f7c1d91e3d8eb13ed4fdd806432e1855d5b3b
JoseHS2002 <joherso16@gmail.com>
2024-10-14 17:58:47 +0200

    Ejercio16

---
+++ src/main/java/com/josehs/tema03/bucles/Ejercicio15.java
@@ -0,0 +1,27 @@
+package com.josehs.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int h=0;
+        int m=0;
+        int s=0;
+
+        System.out.println("Hora: ");
+        h=teclado.nextInt();
+        System.out.println("Minutos: ");
+        m=teclado.nextInt();
+        System.out.println("Segundos: ");
+        s=teclado.nextInt();
+        teclado.close();
+
+        if ( !(h>=0 && h<=23) || !(m>=0 && m<=59) || !(s>=0 && s<=59)){
+            System.out.println("La hora no es valida");
+        } else
+        {
+            System.out.println("La hora es valida");
+        }
+    }
+}

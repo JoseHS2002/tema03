344f932857bdd23769ad43569363f2ed144c5a6d
JoseHS2002 <joherso16@gmail.com>
2024-10-15 14:06:32 +0200

    Ejercio3

--- src/main/java/com/josehs/tema03/ANSI/Ejercicio4.java
+++ src/main/java/com/josehs/tema03/ANSI/Ejercicio4.java
@@ -1,42 +1,33 @@
 package com.josehs.tema03.ANSI;
 
-public class Ejercicio3 {
+public class Ejercicio4 {
     public static void main(String[] args) {
-
         int a=1;
-        System.out.print("        ");
         System.out.println(a);
-        System.out.print("       ");
         for (int b=1; b<=2; b++){
             System.out.print(b);
         }
         System.out.println("");
-        System.out.print("      ");
         for (int c=1; c<=3; c++){
             System.out.print(c);
         }
         System.out.println("");
-        System.out.print("     ");
         for (int d=1; d<=4; d++){
             System.out.print(d);
         }
         System.out.println("");
-        System.out.print("    ");
         for (int e=1; e<=5; e++){
             System.out.print(e);
         }
         System.out.println("");
-        System.out.print("   ");
         for (int f=1; f<=6; f++){
             System.out.print(f);
         }
         System.out.println("");
-        System.out.print("  ");
         for (int g=1; g<=7; g++){
             System.out.print(g);
         }
         System.out.println("");
-        System.out.print(" ");
         for (int h=1; h<=8; h++){
             System.out.print(h);
         }
@@ -44,6 +35,5 @@ public class Ejercicio3 {
         for (int i=1; i<=9; i++){
             System.out.print(i);
         }
-
     }
 }

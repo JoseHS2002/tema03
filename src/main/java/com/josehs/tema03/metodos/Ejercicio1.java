c97f8bf1c583523335767ba7fb347365b5876e00
JoseHS2002 <joherso16@gmail.com>
2024-10-23 11:28:43 +0200

    Ejercio3

--- src/main/java/com/josehs/tema03/metodos/Ejercicio1.java
+++ src/main/java/com/josehs/tema03/metodos/Ejercicio1.java
@@ -2,9 +2,12 @@ package com.josehs.tema03.metodos;
 
 public class Ejercicio1 {
 
-        public static float sumar (float x, float y){
-                float resultadoSuma=sumar( 4, 5);
+        public static int sumar (int x, int y){
+                int resultadoSuma=sumar( 4, 5);
                 return x+y;
-                System.out.println(resultadoSuma);
+        }
+
+        public static void main(String[] args) {
+
         }
 }
